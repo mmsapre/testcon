97a4ecd307df879908ae27d8120a3fa6cbbf8f30
mmsapre <maheshsapre@gmail.com>
2025-10-26 12:30:04 +0530

    Update JmsConfig.java

--- JmsConfig.java
+++ JmsConfig.java
@@ -1,45 +1,61 @@
+package com.example.jms.config;
 
 import com.example.jms.alert.AlertNotifier;
 import com.example.jms.metrics.SolaceConnectionMetrics;
+import com.example.jms.support.ConnectionResetHandler;
 import org.apache.camel.component.jms.JmsComponent;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.connection.CachingConnectionFactory;
 
-/** Camel JMS configuration using resilient Solace connection factory. */
 @Configuration
 public class JmsConfig {
 
     @Bean
-    public CachingConnectionFactory cachingConnectionFactory() {
-        CachingConnectionFactory cf = new CachingConnectionFactory();
-        cf.setSessionCacheSize(10);
-        cf.setReconnectOnException(true);
-        cf.setCacheConsumers(false);
-        return cf;
+    public SolaceConnectionMetrics solaceMetrics() {
+        return new SolaceConnectionMetrics();
     }
 
     @Bean
-    public MultiHostFailoverConnectionFactory solaceFactory(
+    public AlertNotifier alertNotifier() {
+        return new AlertNotifier();
+    }
+
+    @Bean
+    public MultiHostFailoverConnectionFactory solaceConnectionFactory(
             SolaceConnectionMetrics metrics,
             AlertNotifier notifier,
-            CachingConnectionFactory cachingFactory,
             @Value("${solace.host}") String hosts,
             @Value("${solace.vpn}") String vpn,
             @Value("${solace.username}") String user,
-            @Value("${solace.password}") String pass
-    ) {
+            @Value("${solace.password}") String pass) {
+
         return new MultiHostFailoverConnectionFactory(
-                hosts, vpn, user, pass, metrics, notifier, cachingFactory, 5, 5000
+                hosts, vpn, user, pass,
+                metrics, notifier,
+                5, 3000   // retry 5 times, 3s delay
         );
     }
 
     @Bean
-    public JmsComponent jms(MultiHostFailoverConnectionFactory solaceFactory,
-                            CachingConnectionFactory cachingFactory) {
-        cachingFactory.setTargetConnectionFactory(solaceFactory);
-        cachingFactory.setReconnectOnException(true);
-        return JmsComponent.jmsComponent(cachingFactory);
+    public CachingConnectionFactory cachingConnectionFactory(
+            MultiHostFailoverConnectionFactory multiHostFactory) {
+
+        CachingConnectionFactory caching = new CachingConnectionFactory();
+        caching.setTargetConnectionFactory(multiHostFactory);
+        caching.setSessionCacheSize(10);
+        caching.setReconnectOnException(true);
+
+        // Bind callback (no circular dependency)
+        ConnectionResetHandler handler = caching::resetConnection;
+        multiHostFactory.setResetHandler(handler);
+
+        return caching;
+    }
+
+    @Bean(name = "jms")
+    public JmsComponent jms(CachingConnectionFactory cachingConnectionFactory) {
+        return JmsComponent.jmsComponent(cachingConnectionFactory);
     }
 }
